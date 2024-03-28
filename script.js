e44b8c2071c0da9984e7406fe71661e230e038f1
dragosverdes <147598784+dimaiisigma@users.noreply.github.com>
2024-03-28 13:56:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var nav = document.getElementById("side-menu");
+    nav.classList.toggle("active");
+    var burgerMenu = document.querySelector(".burger-menu");
+    burgerMenu.classList.toggle("active");
+}
